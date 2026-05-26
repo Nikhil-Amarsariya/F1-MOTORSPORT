243d41bc06d51cf015971bb3fcf26f29d2902d6a
Nikhil Amarsariya <yourgithubemail@example.com>
2026-05-26 16:07:25 +0530

    Fix API CORS

---
+++ api/news.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const query = req.query.q || "formula 1";
+
+  try {
+    const response = await fetch(
+      `https://gnews.io/api/v4/search?q=${query}&lang=en&max=10&apikey=${process.env.GNEWS_API_KEY}`
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({
+      error: "Failed to fetch news",
+    });
+  }
+}
--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,9 +1,7 @@
 export async function fetchNews() {
   try {
     const response = await fetch(
-      `https://gnews.io/api/v4/search?q=formula%201&lang=en&max=10&apikey=${
-        import.meta.env.VITE_GNEWS_API
-      }`
+      `/api/news?q=formula 1`
     );
 
     const data = await response.json();
