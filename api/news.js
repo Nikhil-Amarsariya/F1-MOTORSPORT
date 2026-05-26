f98f88dfcb536a0ae6225e4b5a2b64847fc60308
Nikhil Amarsariya <yourgithubemail@example.com>
2026-05-26 16:20:20 +0530

    Fix Vercel serverless function

--- api/news.js
+++ api/news.js
@@ -3,7 +3,9 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://gnews.io/api/v4/search?q=${query}&lang=en&max=10&apikey=${process.env.GNEWS_API_KEY}`
+      `https://gnews.io/api/v4/search?q=${encodeURIComponent(
+        query
+      )}&lang=en&max=10&apikey=${process.env.GNEWS_API_KEY}`
     );
 
     const data = await response.json();
